0588ae1ceba2b82bae7aeb28298728e41010a10f
Garfield <matias.sepulveda.bustamante@gmail.com>
2023-03-26 23:09:58 -0300

    footer with automatic year change

--- app.js
+++ app.js
@@ -10,3 +10,8 @@ CardHover();
 
 // AOS library
 AOS.init();
+
+// Add year to footer
+const footerY = document.querySelector('.footer-year');
+const year = new Date();
+footerY.innerHTML = year.getFullYear();
